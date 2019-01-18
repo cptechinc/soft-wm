4c55cd698a96e76eb333e9da777ceed66977ce0d
bbullemer <barbara.bullemer@gmail.com>
2019-01-18 13:30:05 -0600

    Include page files
    - copied from portal files

--- site/templates/content/common/include-toolbar-page.php
+++ site/templates/content/common/include-toolbar-page.php
@@ -3,4 +3,4 @@
         <h1 class="font-weight-bold border-bottom border-primary pb-2 mb-4"><?= $page->get('pagetitle|headline|title') ; ?></h1>
         <?php include $page->body; ?>
     </div>
-<?php include($config->paths->templates.'_foot.php'); // include footer markup ?>
+<?php include($config->paths->templates.'_foot-with-toolbar.php'); // include footer markup ?>
